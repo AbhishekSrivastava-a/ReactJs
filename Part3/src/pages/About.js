a4aabc7e10bf63f40f74dcce27ad97fc1e7ea612
Abhishek Srivastava <avix1011.a@gmail.com>
2025-07-25 17:25:12 +0530

    Foodor

--- Part3/src/pages/About.js
+++
@@ -1,9 +0,0 @@
-const About = () => {
- return (
-  <div>
-   <h2>I am About section</h2>
-  </div>
- )
-}
-
-export default About;
--- Part3/src/pages/Offers.js
+++
@@ -1,9 +0,0 @@
-const Offers = () => {
- return (
-  <div>
-   <h2>I am Offers section</h2>
-  </div>
- )
-}
-
-export default Offers;
